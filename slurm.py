0b7af7153d0f4ea4c764f5e009595fa084744483
Subha Maity <smaity@smaity-mbp.local>
2021-03-11 18:07:34 -0500

    Edit slurm job

--- slurm.py
+++ slurm.py
@@ -14,7 +14,7 @@ parameters = grid(n_signal = pars['n_signal'], n_pi = pars['n_pi'],\
 n_sims = len(parameters)
 n_jobs = int(n_sims / 5000) + 1
 job_log_file = 'job-logs.txt'
-
+last_array_size = n_sims - int(n_sims / 5000) * 5000
 for job in range(n_jobs):
 
     job_file = f'job_files/job_{job}.sh'
@@ -23,7 +23,10 @@ for job in range(n_jobs):
     job_string = f'#!/bin/bash\n'
     job_string += f'#SBATCH --job-name=sim{job}\n'
     job_string += f'#SBATCH --output=logs/sim_%A_%a.out\n'
-    job_string += f'#SBATCH --array=0-4999\n'
+    if job < (n_jobs - 1):
+        job_string += f'#SBATCH --array=0-4999\n'
+    else: 
+        job_string += f'#SBATCH --array=0-{last_array_size}\n'
     job_string += f'#SBATCH --nodes=1\n#SBATCH --cpus-per-task=1\n#SBATCH --mem-per-cpu=6gb\n'
     job_string += f'#SBATCH --time=1:00:00\n#SBATCH --account=yuekai1\n#SBATCH --mail-type=NONE\n'
     job_string += f'#SBATCH --mail-user=smaity@umich.edu\n#SBATCH --partition=standard\n'
